ddd9d17a8e76d5d48b960327a571c91152701862
201810773 <sjo5525@naver.com>
2024-06-16 20:43:14 +0900

    change ETL

--- dags/GMP_Parking.py
+++ dags/GMP_Parking.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 from airflow.utils.dates import days_ago
 from datetime import datetime, timedelta
@@ -8,9 +8,10 @@ import pandas as pd
 import xml.etree.ElementTree as ET
 from airflow.models import Variable
 from plugins import slack
-import json
+import pytz
 
 
+# Function to fetch XML data from the API
 def fetch_xml_data(airport_code):
     url = 'http://openapi.airport.co.kr/service/rest/AirportParkingCongestion/airportParkingCongestionRT'
     parking_api_key = Variable.get('parking_api_key')
@@ -25,11 +26,13 @@ def fetch_xml_data(airport_code):
     response.raise_for_status()
     return response.text
 
+
+# Function to calculate parking congestion degree and status
+
 def calculate_date(sysGetdate, sysGettime):
     datetm = f"{sysGetdate[:4]}{sysGetdate[5:7]}{sysGetdate[8:]}{sysGettime[:2]}{sysGettime[3:5]}{sysGettime[6:]}"
     return datetm
 
-# Function to fetch parking data for a specific airport code
 def parse_xml_data(xml_data):
     root = ET.fromstring(xml_data)
     items = []
@@ -56,10 +59,47 @@ def update_parking_data():
     parsed_data_gmp = parse_xml_data(data_gmp)
     df = pd.DataFrame(parsed_data_gmp)
     df.to_csv('/tmp/GMP_parking_data.csv', index=False, encoding='utf-8-sig')
-    print("Domestic data fetched and saved to '/tmp/GMP_parking_data.csv'")
+    print("Domestic data fetched and saved to '/tmp/CJU_parking_data.csv'")
 
 
-# Default settings for the DAG
+# Convert UTC to KST
+def convert_to_kst(execution_date):
+    # UTC에서 한국 시간으로 변환
+    execution_date_utc = execution_date.replace(tzinfo=pytz.UTC)
+    execution_date_kst = execution_date_utc.astimezone(pytz.timezone('Asia/Seoul'))
+    return execution_date_kst.strftime('%Y-%m-%d %H:%M:%S')
+
+def set_kst_execution_date(**kwargs):
+    # Airflow의 execution_date를 가져와서 한국 시간대로 변환 후 XCom에 저장
+    execution_date = kwargs['execution_date']
+    execution_date_kst = convert_to_kst(execution_date)
+    ti = kwargs['ti']
+    ti.xcom_push(key='execution_date_kst', value=execution_date_kst)
+
+# Function to retrieve the KST execution date from XCom and format the destination path
+def get_kst_execution_date_path(**kwargs):
+    ti = kwargs['ti']
+    execution_date_kst = ti.xcom_pull(task_ids='set_kst_execution_date', key='execution_date_kst')
+    # Convert the pulled execution_date_kst string back to a datetime object
+    execution_date_kst_dt = datetime.strptime(execution_date_kst, '%Y-%m-%d %H:%M:%S')
+    formatted_day = execution_date_kst_dt.strftime("%d")
+    formatted_timestamp = execution_date_kst_dt.strftime("%Y%m%d%H%M")
+    return f'source/source_parkinglot/GMP_parking_data/2024/06/{formatted_day}/GMP_parking_data_{formatted_timestamp}.csv'
+
+def upload_to_gcs_callable(**kwargs):
+    dst_path = get_kst_execution_date_path(**kwargs)
+    # Use the path in LocalFilesystemToGCSOperator
+    LocalFilesystemToGCSOperator(
+        task_id='upload_GMP_parking_data',
+        src='/tmp/GMP_parking_data.csv',
+        dst=dst_path,
+        bucket='pdc3project-landing-zone-bucket',
+        gcp_conn_id='google_cloud_GCS',
+        dag=kwargs['dag'],
+    ).execute(kwargs)
+
+
+# Define the default_args for the DAG
 default_args = {
     'owner': 'airflow',
     'start_date': days_ago(1),
@@ -70,27 +110,31 @@ default_args = {
 
 # Define the DAG
 dag = DAG(
-    'fetch_and_GMP_parking_data',
+    'update_GMP_parking_data',
     default_args=default_args,
-    description='Fetch parking data from OpenAPI for GMP and CJU and store in GCS every 5 minutes',
+    description='A DAG to update parking data every 5 minutes and save it to GCS',
     schedule_interval='*/5 * * * *',
     catchup=False,
 )
 
-# Define the task
 fetch_and_upload_task = PythonOperator(
     task_id='fetch_and_upload_GMP_parking_data',
     python_callable=update_parking_data,
     dag=dag,
 )
 
-upload_to_gcs_task = LocalFilesystemToGCSOperator(
-    task_id='upload_GMP_parking_data', # 개발자의 task 정의에 따라 임의로 입력
-    src='/tmp/GMP_parking_data.csv', # API에서 받아온 데이터를 로컬에 csv로 먼저 저장한 상황을 가정하며 해당 csv파일 위치 입력
-    dst='source/source_parkinglot/GMP_parking_data/2024/06/{{ execution_date.strftime("%d") }}/GMP_parking_data_{{ execution_date.strftime("%Y%m%d") }}.csv',
-    bucket='pdc3project-landing-zone-bucket',
-    gcp_conn_id='google_cloud_GCS',
+set_kst_task = PythonOperator(
+    task_id='set_kst_execution_date',
+    python_callable=set_kst_execution_date,
+    provide_context=True,
     dag=dag,
 )
 
-fetch_and_upload_task>>upload_to_gcs_task
+upload_to_gcs_task = PythonOperator(
+    task_id='upload_GMP_airplane_parking_data',
+    python_callable=upload_to_gcs_callable,
+    provide_context=True,
+    dag=dag,
+)
+
+fetch_and_upload_task >> set_kst_task >> upload_to_gcs_task
---
+++ dags/Parking_INT_ELT.py
@@ -0,0 +1,140 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.google.cloud.hooks.gcs import GCSHook
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.utils.dates import days_ago
+from datetime import datetime, timedelta
+import pyarrow.parquet as pq
+import pandas as pd
+import pyarrow as pa
+from plugins import slack
+import pytz
+import re
+
+
+def convert_to_kst(execution_date):
+    # UTC에서 한국 시간으로 변환
+    execution_date_utc = execution_date.replace(tzinfo=pytz.UTC)
+    execution_date_kst = execution_date_utc.astimezone(pytz.timezone('Asia/Seoul'))
+    return execution_date_kst.strftime('%Y-%m-%d %H:%M:%S')
+
+def set_kst_execution_date(**kwargs):
+    # Airflow의 execution_date를 가져와서 한국 시간대로 변환 후 XCom에 저장
+    execution_date = kwargs['execution_date']
+    execution_date_kst = convert_to_kst(execution_date)
+    ti = kwargs['ti']
+    ti.xcom_push(key='execution_date_kst', value=execution_date_kst)
+
+def extract_csv_from_gcs(execution_date_str, **kwargs):
+    ti = kwargs['ti']
+    execution_date_kst = ti.xcom_pull(task_ids='set_kst_execution_date', key='execution_date_kst')
+    # Convert the pulled execution_date_kst string back to a datetime object
+    execution_date_kst_dt = datetime.strptime(execution_date_kst, '%Y-%m-%d %H:%M:%S')
+    formatted_day = execution_date_kst_dt.strftime("%d")
+    formatted_timestamp = execution_date_kst_dt.strftime("%Y%m%d%H")
+    gcs_hook = GCSHook(gcp_conn_id='google_cloud_GCS')
+    bucket_name = 'pdc3project-landing-zone-bucket'
+
+    # GCS 객체의 prefix와 정규 표현식 패턴을 설정
+    prefix = f'source/source_parkinglot/INT_parking_data/2024/06/{formatted_day}'
+    regex_pattern = f'INT_parking_data_{formatted_timestamp}\d{{2}}.csv'
+
+    # 지정된 GCS prefix에서 모든 파일 리스트를 가져옴
+    objects = gcs_hook.list(bucket_name, prefix=prefix)
+
+    # 정규 표현식 패턴과 일치하는 파일을 찾음
+    matching_files = [obj for obj in objects if re.search(regex_pattern, obj)]
+
+    if not matching_files:
+        raise FileNotFoundError(f"No files matching pattern {regex_pattern} in {prefix}")
+
+    # 첫 번째로 일치하는 파일을 다운로드
+    object_name = matching_files[0]
+    local_path = '/tmp/INT_parking_data.csv'
+    gcs_hook.download(bucket_name, object_name, local_path)
+
+
+def transform_csv_to_parquet(execution_date_str, **kwargs):
+    # 로컬에 저장된 CSV 파일 경로를 설정
+    local_csv_path = '/tmp/INT_parking_data.csv'
+    df = pd.read_csv(local_csv_path)
+
+    # Parquet 파일로 저장할 경로를 설정
+    parquet_path = '/tmp/INT_parking_data.parquet'
+    table = pa.Table.from_pandas(df)
+    pq.write_table(table, parquet_path)
+
+    # 현재 태스크 인스턴스를 나타내는 task_instance 객체를 가져옴
+    ti = kwargs['ti']
+
+    # XCom을 사용하여 Parquet 파일 경로를 push(저장)
+    ti.xcom_push(key='parquet_path', value=parquet_path)
+
+
+def upload_to_gcs(execution_date_str, **kwargs):
+    # 현재 태스크 인스턴스를 나타내는 task_instance 객체를 가져옴
+    ti = kwargs['ti']
+    execution_date_kst = ti.xcom_pull(task_ids='set_kst_execution_date', key='execution_date_kst')
+    # Convert the pulled execution_date_kst string back to a datetime object
+    execution_date_kst_dt = datetime.strptime(execution_date_kst, '%Y-%m-%d %H:%M:%S')
+    formatted_day = execution_date_kst_dt.strftime("%d")
+    formatted_timestamp = execution_date_kst_dt.strftime("%Y%m%d%H%M")
+    # 이전 태스크에서 저장된 Parquet 파일 경로를 XCom을 통해 가져옴
+    parquet_path = ti.xcom_pull(key='parquet_path', task_ids='transform_csv_to_parquet')
+
+    # execution_date_str을 datetime 객체로 변환
+    execution_date = datetime.strptime(execution_date_str, '%Y-%m-%dT%H:%M:%S%z')
+    bucket = 'pdc3project-stage-layer-bucket'
+    gcs_hook = GCSHook(gcp_conn_id='google_cloud_GCS')
+    dst_path = f'source/source_parkinglot/INT_parking_data/2024/06/{formatted_day}/INT_parking_data_{formatted_timestamp}.parquet'
+
+    # Parquet 파일을 GCS에 업로드
+    gcs_hook.upload(bucket_name=bucket, object_name=dst_path, filename=parquet_path)
+
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+    'on_failure_callback': slack.on_failure_callback,
+}
+
+dag = DAG(
+    'INT_csv_to_parquet',
+    default_args=default_args,
+    description='CSV 데이터를 Parquet으로 변환하여 GCS에 저장하는 DAG',
+    schedule_interval='*/5 * * * *',
+    max_active_runs=1,
+    catchup=False,
+)
+
+set_kst_task = PythonOperator(
+    task_id='set_kst_execution_date',
+    python_callable=set_kst_execution_date,
+    provide_context=True,
+    dag=dag,
+)
+
+extract_task = PythonOperator(
+    task_id='extract_csv_from_gcs',
+    python_callable=extract_csv_from_gcs,
+    op_kwargs={'execution_date_str': '{{ ts }}'},  # ts는 실행 날짜와 시간을 문자열로 반환합니다.
+    dag=dag,
+)
+
+transform_task = PythonOperator(
+    task_id='transform_csv_to_parquet',
+    python_callable=transform_csv_to_parquet,
+    op_kwargs={'execution_date_str': '{{ ts }}'},
+    dag=dag,
+)
+
+upload_task = PythonOperator(
+    task_id='upload_to_gcs',
+    python_callable=upload_to_gcs,
+    op_kwargs={'execution_date_str': '{{ ts }}'},
+    dag=dag,
+)
+
+set_kst_task >> extract_task >> transform_task >> upload_task
